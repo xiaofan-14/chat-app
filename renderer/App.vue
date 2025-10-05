3942baba7f974ef06a18117b37739db69f04bbae
xiaofan <ningfengzeng@gmail.com>
2025-10-05 22:11:48 +0800

    feat: use naive-ui and tailwindcss

--- renderer/App.vue
+++ renderer/App.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { NConfigProvider } from 'naive-ui'
 
-const count = ref(0);
-function handleClick() {
-  count.value++;
-}
 </script>
 <template>
-  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
-    <h1 class="text-2xl font-bold text-center mb-1">Hello World</h1>
-    <p>click this button to increment the count</p>
-    <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      @click="handleClick"
-    >
-      count: {{ count }}
-    </button>
-  </div>
+  <NConfigProvider class="h-[100vh] w-[100vw] flex">
+    <aside class="h-full flex flex-shrink-0 flex-col bg-red-500">
+      Aside
+    </aside>
+    <div class="flex-auto">
+      Main
+    </div>
+  </NConfigProvider>
 </template>
