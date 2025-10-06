cdffab6fd7fabc9e7f74f6dbee7ee9c4235a16fb
xiaofan <ningfengzeng@gmail.com>
2025-10-06 14:52:25 +0800

    feat: add custom title bar

--- renderer/App.vue
+++ renderer/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-import { NConfigProvider } from 'naive-ui'
-
+import { NConfigProvider } from "naive-ui";
+import TitleBar from "./components/title-bar.vue";
+import DragRegion from "./components/drag-region.vue";
 </script>
+
 <template>
   <NConfigProvider class="h-[100vh] w-[100vw] flex">
-    <aside class="h-full flex flex-shrink-0 flex-col bg-red-300 px-2">
+    <aside class="h-full flex flex-shrink-0 flex-col bg-red-300 px-2 w-[200px]">
       Aside
     </aside>
     <div class="flex-auto">
+      <TitleBar>
+        <DragRegion class="w-full"/>
+      </TitleBar>
       Main
     </div>
   </NConfigProvider>
---
+++ renderer/hooks/use-window-manager.ts
@@ -0,0 +1,30 @@
+export function useWindowManager() {
+  const isMaximized = ref(false)
+
+  function closeWindow() {
+    window.api.closeWindow();
+  }
+
+  function minimizeWindow() {
+    window.api.minimizeWindow();
+  }
+
+  function maximizeWindow() {
+    window.api.maximizeWindow();
+  }
+
+  onMounted(async () => {
+    await nextTick();
+    isMaximized.value = await window.api.isWindowMaximized();
+    window.api.onWindowMaximized((_isMaximized: boolean) => isMaximized.value = _isMaximized);
+  })
+
+  return {
+    isMaximized,
+    closeWindow,
+    minimizeWindow,
+    maximizeWindow
+  }
+};
+
+export default useWindowManager;
